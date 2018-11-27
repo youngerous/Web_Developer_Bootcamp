5568df6f03581eb0644493e5cece6e2c1d40ca01
young <youngerous@gmail.com>
2018-11-28 05:50:01 +0900

    Data Associations

---
+++ week7/Associations/references.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/blog_demo_2');
+var Post = require('./models/post');
+var User = require('./models/user');
+
+
+Post.create({
+    title: 'How to cook the best burger Pt.4',
+    content: 'hello korea'
+}, function(err, post){
+    User.findOne({email:'bob@gmail.com'}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            foundUser.posts.push(post);
+            foundUser.save(function(err, data){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log(data);
+                }
+            });
+        }
+    });
+});
+
+// User.create({
+//     email: 'bob@gmail.com',
+//     name: 'Bob Belcher',
+// });
+
+// // Find user
+// User.findOne({email: 'bob@gmail.com'}).populate('posts').exec(function(err, user){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log(user);
+//     }
+// });
+// find all posts for that user
