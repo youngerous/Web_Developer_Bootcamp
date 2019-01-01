6e8cbc2052c4beaa30cbb3fa352cb241376fa147
young <youngerous@gmail.com>
2019-01-02 00:48:03 +0900

    authentication demo

---
+++ week6/YelpCamp/models/campground.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+module.exports = mongoose.model("Campground", campgroundSchema);
+
+// 이 파일을 require하면 해당 model을 얻게 된다.
---
+++ week6/YelpCamp/seeds.js
@@ -0,0 +1,59 @@
+var mongoose = require('mongoose');
+var Campground = require('./models/campground');
+var Comment = require('./models/comment');
+
+var data = [
+    {
+        name: "Cloud's REST",
+        image: "https://media.gettyimages.com/photos/aerial-view-of-clouds-over-green-landscape-picture-id600825209",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+    },
+    {
+        name: "CAT CUTE",
+        image: "https://media.gettyimages.com/photos/closeup-of-cats-yawning-against-white-background-picture-id961466534",
+        description: "my cat"
+    },
+    {
+        name: "Students..",
+        image: "https://media.gettyimages.com/photos/business-woman-working-on-desk-picture-id850630688",
+        description: "getting a job is hard"
+    }
+]
+
+
+function seedDB(){
+    // Remove all campgrounds
+    Campground.remove({},function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log('removed campgrounds!');
+        // Add few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err);
+                }else{
+                    console.log("added a campground");
+                    // Add few comments
+                    Comment.create({
+                       text: "This is good , but I wish there was an Internet" ,
+                       author: "Homer"
+                    }, function(err, comment){
+                        if(err){
+                            console.log(err);
+                        }else{
+                            campground.comments.push(comment);  
+                            campground.save();
+                            console.log("Created new comment");
+                        }
+                    });
+                }
+            });
+        });
+    });
+
+
+}
+
+module.exports = seedDB;
